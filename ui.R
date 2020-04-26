51d40d1c7bb55e8ae5f052e347345985193ec1dc
TMax66 <massimo.tranquillo@gmail.com>
2020-04-26 23:15:59 +0200

    update

--- ui.R
+++ ui.R
@@ -1,9 +1,9 @@
-ui<-
-  fluidPage(theme = shinytheme("cerulean"))
-
-
-navbarPage("IZSLER- Sez. di Bergamo - SAQ",
+ui<-navbarPage("IZSLER- Sez. di Bergamo - SAQ",
+           theme = shinytheme("cerulean"),
            tabPanel("Documentazione locale",
+                    column(12,
+                    fluidRow(
+                    h4( "Personale"),
                     a(actionButton("SK", "Schede individuali",
                                    class = "btn-primary",
                                    icon("address-card")),
@@ -13,7 +13,14 @@ navbarPage("IZSLER- Sez. di Bergamo - SAQ",
                             class = "btn-primary",
                       icon("vials")),
                      href="https://izsler-my.sharepoint.com/:f:/g/personal/vito_tranquillo_izsler_it/Et3U11eBqS1Hj-RcsczKOLgBJ6pt1pc9xj8puuQASHuXBA?e=zCFAPd"),
-               
+                    )),
+              
+                hr(),
+                br(),
+ 
+                column(12, 
+                       fluidRow( 
+                         h4("Documentazione organizzativa"),
                 a(actionButton("M", "Moduli",
                               class = "btn-primary",
                               icon("file-alt")),
@@ -22,30 +29,39 @@ navbarPage("IZSLER- Sez. di Bergamo - SAQ",
                 a(actionButton("IO", "Istruzioni Operative",
                                class = "btn-primary",
                                icon("tasks")),
-                  href="https://izsler-my.sharepoint.com/:f:/g/personal/vito_tranquillo_izsler_it/Eg1cfmMP4SVEjlVFlVL_wnIBAJ8ih-2mCXQw89U__qXXUg?e=Rqimnq")
+                  href="https://izsler-my.sharepoint.com/:f:/g/personal/vito_tranquillo_izsler_it/Eg1cfmMP4SVEjlVFlVL_wnIBAJ8ih-2mCXQw89U__qXXUg?e=Rqimnq"),
                 
+                a(actionButton("daut", "Documenti autoportanti",
+                               class = "btn-primary",
+                               icon("tasks")),
+                  href="https://izsler-my.sharepoint.com/:f:/g/personal/vito_tranquillo_izsler_it/Eu70iDSPr_dHrkJc6oTizSYBKtjm9-GBbxWYPw0I30Nkag?e=0V2o3C")
                 
-               
+                       )
                 
-                
-                
-                
-                    ), 
+                )
+                    ),
            
-           
-           
-           
-           
-           
+           tabPanel("Tools",
+                    fluidRow(
+                      column(12, 
+                      a(actionButton("CC", "Carte di Controllo",
+                                     class = "btn-primary",
+                                     icon("chart-bar")),
+                        href="https://izslerbg.shinyapps.io/CCbergamo/"),
+
+                      a(actionButton("pg85", "PG00/085",
+                                     class = "btn-primary",
+                                     icon("chart-bar")),
+                        href="https://izslerapp.shinyapps.io/PG85E/")
+   
+                    )
+                    )
+                    ),
            tabPanel("Archivio NC",
                     fluidPage(
                       DT::dataTableOutput("nc")
                     )
-                    ) 
+           )
+           
                     )
 
- # tags$a(href="https://izsler-my.sharepoint.com/:f:/g/personal/vito_tranquillo_izsler_it/Et3U11eBqS1Hj-RcsczKOLgBJ6pt1pc9xj8puuQASHuXBA?e=zCFAPd", "Qualificazioni/Mantenimenti")
-
-  
-
-  
